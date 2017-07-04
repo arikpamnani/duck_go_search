e488362c573e1ddfab31a5568ef0d423e18d8365
Arik Pamnani <arikpamnani@gmail.com>
2017-07-05 01:14:11 +0530

    +5

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='duck-go',
+   name='duck_go',
    version='1.0',
    description='A command line utility for search on duckduckgo.com',
    author='Arik Pamnani',
    author_email='arikpamnani@gmail.com',
-   packages=['duck-go'],  #same as name
-   install_requires=['lxml', 'requests', 'urllib'], #external packages as dependencies
+   packages=['duck_go'],  #same as name
+   install_requires=[], #external packages as dependencies
 )
